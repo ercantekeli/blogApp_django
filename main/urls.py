057c176d646581fce11cc459acbf506dacc03864
ercantekeli <ercantekeli@outlook.com>
2022-06-05 22:11:40 +0300

    last commit

--- main/urls.py
+++ main/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('users/', include('users.urls')),     
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
